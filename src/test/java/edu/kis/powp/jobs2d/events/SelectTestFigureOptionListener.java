ccbb34065ab2f407464781278278e8e13ca93c77
MichalC <m.colautti@o2.pl>
2025-03-17 17:38:44 +0100

    Dodanie klasy AbstractDriverAdapter oraz testu wyświetlającego wzorce demonstracyjne z klasy FiguryJane

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,9 +2,10 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Objects;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.adapter.AbstractDriverAdapter;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -22,6 +23,10 @@ public class SelectTestFigureOptionListener implements ActionListener {
 		}
 		else if(e.getActionCommand().equals("Figure Joe 2")) {
 			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
+		else if(e.getActionCommand().equals("Figure Jane")) {
+			AbstractDriverAdapter driver = new AbstractDriverAdapter(driverManager.getCurrentDriver());
+			FiguresJane.figureScript(driver);
 		}
 	}
 }
