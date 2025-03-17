a1b5b030851595f2d7c2d92b801d8f15a29bf1ca
MichalC <m.colautti@o2.pl>
2025-03-17 16:16:59 +0100

    Implementacja LineDrawerAdapter oraz dodanie możliwość korzystania z zaimplementowanego adaptera

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -7,8 +7,10 @@ import java.util.logging.Logger;
 
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.Job2dDriverAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -42,6 +44,15 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new Job2dDriverAdapter(DrawerFeature.getDrawerController());
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver straightLineDriver = new LineDrawerAdapter(DrawerFeature.getDrawerController(), LineFactory.getBasicLine());
+		DriverFeature.addDriver("Straight line driver", straightLineDriver);
+
+		Job2dDriver dottedLineDriver = new LineDrawerAdapter(DrawerFeature.getDrawerController(), LineFactory.getDottedLine());
+		DriverFeature.addDriver("Dotted line driver", dottedLineDriver);
+
+		Job2dDriver specialLineDriver = new LineDrawerAdapter(DrawerFeature.getDrawerController(), LineFactory.getSpecialLine());
+		DriverFeature.addDriver("Special line driver", specialLineDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
